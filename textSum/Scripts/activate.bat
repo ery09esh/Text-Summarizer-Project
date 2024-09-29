9184b0d497a0011c5e9e8e29295c2649cc80a4d2
ery09esh <yogesh.nutanvij@gmail.com>
2024-09-29 17:36:52 +0530

    model evaluation completed

--- textSum/Scripts/activate.bat
+++ textSum/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\mlops\Github Action CICD pipelines\text_summarizer_project\Text-Summarizer-Project\textSum
+set VIRTUAL_ENV=D:\mlops\Github Action CICD pipelines\text_summarizer_project\Text-Summarizer-Project\textsum
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(textSum) %PROMPT%
+set PROMPT=(textsum) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(textSum) 
+set VIRTUAL_ENV_PROMPT=(textsum) 
 
 :END
 if defined _OLD_CODEPAGE (
